3412f8a8fa9ec5579f9594e9a41233b21f3dd27f
Marcus Griep <mgriep@cimpress.com>
2017-04-10 21:57:44 -0400

    Initial commit

---
+++ content/Api.fs
@@ -0,0 +1,31 @@
+module Api
+
+open Freya.Core
+open Freya.Optics.Http
+open Freya.Types.Http
+open Freya.Machines
+open Freya.Machines.Http
+open Freya.Routers.Uri.Template
+
+let routeName = Freya.Optic.get (Route.atom_ "name")
+
+let sayHello = freya {
+  let! nameO = routeName
+  let helloStr =
+    match nameO with
+    | Some name -> sprintf "Hello, %s!" name
+    | None -> "Hello, World!"
+  return Represent.text helloStr
+}
+
+let helloMachine = freyaMachine {
+  methods [GET; HEAD; OPTIONS]
+
+  handleOk sayHello
+}
+
+let router = freyaRouter {
+  resource "/hello{/name}" helloMachine
+}
+
+let root = UriTemplateRouter.Freya router
