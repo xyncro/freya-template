511ff80ded9e6c719ba8dd9f5f441b4860a08a44
Marcus Griep <marcus@griep.us>
2017-09-26 15:40:40 -0400

    Add support for Suave

--- content/Program.fs
+++ content/Program.fs
@@ -1,9 +1,15 @@
 module Program
 
+//#if (framework == "kestrel")
 open KestrelInterop
+//#elseif (framework == "suave")
+open Freya.Core
+open Suave
+//#endif
 
 [<EntryPoint>]
 let main argv =
+//#if (framework == "kestrel")
     let configureApp =
         ApplicationBuilder.useFreya Api.root
 
@@ -11,5 +17,10 @@ let main argv =
     |> WebHost.bindTo [|"http://localhost:5000"|]
     |> WebHost.configure configureApp
     |> WebHost.buildAndRun
+//#elseif (framework == "suave")
+    startWebServer
+        defaultConfig
+        (Owin.OwinApp.ofAppFunc "/" (OwinAppFunc.ofFreya Api.root))
+//#endif
 
     0
